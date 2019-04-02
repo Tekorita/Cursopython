de37cccc5711a93daf50a58bee2856e997d60988
David Palma <davidpalmalugo@gmail.com>
2019-04-02 06:20:20 -0300

    correccion en funcion de palindromo

--- palindromo.py
+++ palindromo.py
@@ -4,8 +4,8 @@ def palindromo(palabra):
 	palabra2 = palabra.replace(' ','')
 	resultado = palabra2[::-1]
 	if resultado == palabra2:
-		print("La palabra es un palindromo gracias")
+		print("La palabra es un palindromo gracias la gerencia")
 	else:
-		print("La palabra no es un palindromo gracias")
+		print("La palabra no es un palindromo gracias la gerencia")
 
 palindromo(palabra)
