194f7fd7bf4330763a923d961e1805e18b14e9ff
David Palma <davidpalmalugo@gmail.com>
2019-05-26 22:29:42 -0400

    modificaciones en librerias.py

--- librerias2.py
+++ librerias2.py
@@ -2,8 +2,8 @@ import sys
 
 if __name__ == '__main__':
 	if len(sys.argv) == 1:
-		print("Es necesario colocar por lo menos un argumento")
+		print("Es necesario colocar por lo menos un argumento como minimo")
 	else:
 		if sys.argv[1] == 'help':
-			print("Puedes contactarme par cualquier duda")
+			print("Puedes contactarme par cualquier duda o inquietud")
 	
