1d065ec9e49278fbbb7e4c0e15a47f7659c61901
Tekorita <davidpalmalugo@gmail.com>
2019-06-19 22:37:44 -0400

    correccion en modulo de concatenacion de strings 2

--- concatenacionStrigns.py
+++ concatenacionStrigns.py
@@ -5,7 +5,7 @@ my_string = 'Hola mundo: "David"'
 course = 'Python 3'
 name = 'David'
 
-final_message1 = 'Nuevo curso o tutorial en este caso de deeee de '+ course + ' por ' + name #1
+final_message1 = 'Nuevo curso o tutorial de:'+ course + ' por ' + name #1
 final_message2 = 'Nuevo curso de %s por %s' %(course,name) #2
 final_message3 = 'Nuevo curso de {} por {}' .format(course,name) #3
 
