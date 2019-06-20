3fd4c00bca1f9f11d3bf99dd5494ddf96ac7e4d3
Tekorita <davidpalmalugo@gmail.com>
2019-06-19 22:41:17 -0400

    correccion de mensaje de salida en palindromo.py

--- palindromo.py
+++ palindromo.py
@@ -4,8 +4,8 @@ def palindromo(palabra):
 	palabra2 = palabra.replace(' ','')
 	resultado = palabra2[::-1]
 	if resultado == palabra2:
-		print("La palabra es un palindromo gracias la gerencia")
+		print("La palabra es un palindromo")
 	else:
-		print("La palabra no es un palindromo gracias la gerencia")
+		print("La palabra no es un palindromo")
 
 palindromo(palabra)
