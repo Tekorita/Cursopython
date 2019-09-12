7bbd20d33600f6a608a90effc44bf43fc35eaf9e
Tekorita <davidpalmalugo@gmail.com>
2019-09-11 23:52:44 -0300

    update palindromo

--- palindromo.py
+++ palindromo.py
@@ -4,7 +4,7 @@ def palindromo(palabra):
 	palabra2 = palabra.replace(' ','')
 	resultado = palabra2[::-1]
 	if resultado == palabra2:
-		print("La palabra es un palindromo")
+		print("La palabra es un palindromo he dicho")
 	else:
 		print("La palabra no es un palindromo")
 
