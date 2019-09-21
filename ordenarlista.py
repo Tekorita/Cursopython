cd04300972119abc0c28103e5e7c404876fefbbb
Tekorita <davidpalmalugo@gmail.com>
2019-09-20 23:13:21 -0300

    update file about list desordenada

--- ordenarlista.py
+++ ordenarlista.py
@@ -1,6 +1,6 @@
 lista = [0,6,2,2,5,4,3,1]
 lista3 = []
-print("lista desordenada: "+str(lista))
+print("lista desordenada vuelta mierdddddddd: "+str(lista))
 while lista != []:
     lista3.append(min(lista))
     lista.remove(min(lista))
