7327698369258943cb89ac36d604b882251d81be
Tekorita <davidpalmalugo@gmail.com>
2019-10-04 23:55:56 -0300

    update example in yields

--- generadores.py
+++ generadores.py
@@ -4,7 +4,7 @@
 
 def generador(*args):
 	for valor in args:
-		yield valor * 10, True #Un ejemplo
+		yield valor * 30, True #Un ejemplo
 
 for valor_uno, valor_dos in generador(1,2,3,4,5,6,7,8,9):
 	print(valor_uno, valor_dos) 
