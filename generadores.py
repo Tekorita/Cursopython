c8079ecfac25bda75e9a93a591dc81a216d016eb
Tekorita <davidpalmalugo@gmail.com>
2019-10-04 23:59:40 -0300

    update example in yields 4

--- generadores.py
+++ generadores.py
@@ -12,5 +12,5 @@ for valor_uno, valor_dos in generador(1,2,3,4,5,6,7,8,9):
 for input1, input2 in generador(1,2,3,4):
 	print(valor_uno, valor_dos)
 
-for input3, input4 in generador(1,2):
+for input3, input4 in generador(1,2,3):
 	print(valor_uno, valor_dos) 
