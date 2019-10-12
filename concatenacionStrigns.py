4cef632f72d5d53ffa07fc04af5b9ba795ee4cfc
Tekorita <davidpalmalugo@gmail.com>
2019-10-11 23:58:03 -0300

    add format python3

--- concatenacionStrigns.py
+++ concatenacionStrigns.py
@@ -8,5 +8,6 @@ name = 'David'
 final_message1 = 'Nuevo curso o tutorial de:'+ course + ' por ' + name #1
 final_message2 = 'Nuevo curso de %s por %s' %(course,name) #2
 final_message3 = 'Nuevo curso de {} por {}' .format(course,name) #3
+final_message1 = f'Nuevo curso o tutorial de: {course} por {name}'#1
 
 print(final_message3)
