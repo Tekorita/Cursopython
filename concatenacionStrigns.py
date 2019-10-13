e385ad125a2ec627c725d87937cd608756f2d435
Tekorita <davidpalmalugo@gmail.com>
2019-10-12 23:58:38 -0300

    new implementation in format 3

--- concatenacionStrigns.py
+++ concatenacionStrigns.py
@@ -9,5 +9,6 @@ final_message1 = 'Nuevo curso o tutorial o manual de:'+ course + ' por ' + name 
 final_message2 = 'Nuevo curso o tutorial de %s por %s' %(course,name) #2
 final_message3 = 'Nuevo curso de {} por {}' .format(course,name) #3
 final_message1 = f'Nuevo curso o tutorial de: {course} por {name}'#1
+final_message1 = f'Nuevo curso o tutorial de: {course} por {name}'#1
 
 print(final_message3)
