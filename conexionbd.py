e0280f4399700a84130896ae67f83abe203e9a7b
Tekorita <davidpalmalugo@gmail.com>
2019-10-17 23:48:47 -0300

    delet conection duplicate of bd

--- conexionbd.py
+++ conexionbd.py
@@ -15,19 +15,3 @@ for row in cur:
 
 cur.close()
 conn.close()
-
-import pymysql
-
-conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='', db='prueba')
-
-cur = conn.cursor()
-cur.execute("select * from persona")
-
-print(cur.description)
-print()
-
-for row in cur:
-	print(row)
-
-cur.close()
-conn.close()
