b826c827d373a8418f78543848411f905df79b5c
Tekorita <davidpalmalugo@gmail.com>
2019-10-28 23:52:37 -0300

    update regex example test

--- regex-test.py
+++ regex-test.py
@@ -10,6 +10,5 @@ def Find(string):
       
 # Driver Code 
 string = 'My Profile: https://auth.geeksforgeeks.org / user / Chinmoy % 20Lenka / articles in the portal of http://www.geeksforgeeks.org/' 
-string2 = 'My Profile: https://auth.geeksforgeeks.org / user / Chinmoy % 20Lenka / articles in the portal of http://www.geeksforgeeks.org/' 
  
 print("Urls: ", Find(string))
