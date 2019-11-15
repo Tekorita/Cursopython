c74b405ccaf7d7acf86b72e5280ab65e788326ed
Tekorita <davidpalmalugo@gmail.com>
2019-11-14 23:50:02 -0300

    create superposicion 2

--- prueba3.py
+++ prueba3.py
@@ -13,3 +13,14 @@ list1 = [5,6,7,8]
 list2 = [5,6,7,8]
 
 superposicion(list1,list2)
+
+def superposicion2(lista1, lista2):
+    resultado = " "
+    for valor1 in lista1:
+        for valor2 in lista2:
+            if valor1 == valor2:
+                resultado = "True"                
+            else:
+                resultado = "False"
+            
+            print(resultado) 
