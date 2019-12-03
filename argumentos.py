fc5941afea521e4ed71d42859b13594ef9fb3bed
Tekorita <davidpalmalugo@gmail.com>
2019-12-02 23:53:01 -0300

    update extra paras in args

--- argumentos.py
+++ argumentos.py
@@ -17,7 +17,7 @@ def suma1(*args):
 		resultado = resultado + valor
 	return resultado
 
-resultado = suma1(1,2,3,4,5,6,7,8,9,10,11,12,13)
+resultado = suma1(1,2,3,4,5,6,7,8,9,10)
 print(resultado)
 
 #tambien podemos trabajar con diccionarios y mostrar el nombre de las variables tambien
